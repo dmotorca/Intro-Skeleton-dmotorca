c5b1b6ad191bb5a4d34a91d61363191524af66cb
dmotorca <dmotorca@pdx.edu>
2023-06-15 07:11:12 -0700

    Everything is done, just need to convert to mp4

--- complex_arithmetic.c
+++ complex_arithmetic.c
@@ -5,61 +5,6 @@
 
 #define Wsize 800
 
-// Converts a color from HSV to RGB.
-void HSVtoRGB(double h, double s, double v, double *r, double *g, double *b)
-{
-  int i;
-  double f, p, q, t;
-
-  if (s == 0)
-  {
-    // Achromatic (grey).
-    *r = *g = *b = v;
-    return;
-  }
-
-  h /= 60; // Sector 0 to 5.
-  i = floor(h);
-  f = h - i; // Factorial part of h.
-  p = v * (1 - s);
-  q = v * (1 - s * f);
-  t = v * (1 - s * (1 - f));
-
-  switch (i)
-  {
-  case 0:
-    *r = v;
-    *g = t;
-    *b = p;
-    break;
-  case 1:
-    *r = q;
-    *g = v;
-    *b = p;
-    break;
-  case 2:
-    *r = p;
-    *g = v;
-    *b = t;
-    break;
-  case 3:
-    *r = p;
-    *g = q;
-    *b = v;
-    break;
-  case 4:
-    *r = t;
-    *g = p;
-    *b = v;
-    break;
-  default: // Case 5.
-    *r = v;
-    *g = p;
-    *b = q;
-    break;
-  }
-}
-
 void draw(double rcen, double icen, double rad, double cutoff, int mlimit)
 {
   double delta, x, y, yp, xp;
@@ -89,45 +34,45 @@ void draw(double rcen, double icen, double rad, double cutoff, int mlimit)
       double r, g, b;
       if (reps == mlimit)
       {
-        // Inside the Mandelbrot set.
+        // Inside the set
         r = g = b = 0;
       }
       else
       {
-        // Outside the Mandelbrot set, color based on the number of iterations.
+        // Outside the set, color based is determined by the number of iterations
         double t = (double)reps / mlimit; // Normalized iteration count.
 
         if (t < 1.0 / 6.0)
         {
-          // Black to red transition.
+          // Black to Red
           r = t * 6.0;
           g = 0;
           b = 0;
         }
         else if (t < 1.0 / 3.0)
         {
-          // Red to yellow transition.
+          // Red to Yellow
           r = 1;
           g = (t - 1.0 / 6.0) * 6.0;
           b = 0;
         }
         else if (t < 0.5)
         {
-          // Yellow to green transition.
+          // Yellow to Green
           r = 1 - (t - 1.0 / 3.0) * 6.0;
           g = 1;
           b = 0;
         }
         else if (t < 2.0 / 3.0)
         {
-          // Green to cyan transition.
+          // Green to Cyan(similar?)
           r = 0;
           g = 1;
           b = (t - 0.5) * 6.0;
         }
         else if (t < 5.0 / 6.0)
         {
-          // Cyan to blue transition.
+          // Cyan to blue
           r = 0;
           g = 1 - (t - 2.0 / 3.0) * 6.0;
           b = 1;
@@ -156,7 +101,7 @@ int main()
   double rad = 2.0;
   double cutoff = 100.0;
   int mlimit = 100;
-  double p[2], q[2]; // new addition
+  double p[2], q[2]; // For clicking
 
   G_init_graphics(Wsize, Wsize);
   G_rgb(0.0, 0.0, 0.0);
@@ -170,7 +115,7 @@ int main()
     // Get the coordinates of the two clicked points
     G_wait_click(p);
     G_wait_click(q);
-    // Recalculate the center and radius for zooming
+    // Recalculate the center and radius for zooming each time
     rcen = rcen - rad + (p[0] + q[0]) * rad / Wsize;
     icen = icen - rad + (p[1] + q[1]) * rad / Wsize;
     rad = rad * fabs(p[0] - q[0]) / Wsize;
